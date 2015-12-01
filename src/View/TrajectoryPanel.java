68235fb6c47184289d18dd2838842ff2c97ee7dd
Dfletcher06 <dfletcher06@gmail.com>
2015-12-01 21:27:25 +0000

    Added flag pole

--- src/View/TrajectoryPanel.java
+++ src/View/TrajectoryPanel.java
@@ -84,16 +84,14 @@ public class TrajectoryPanel extends JPanel implements ActionListener{
 		g.setColor(Color.GREEN);
 		g2d.drawLine(0, 0, getWidth(), 0);
 		//if(game.getNumTurns() == 0){
+		//g2d.fillRect(game.randomTargetX(), 0, 10, 10);
 		g.setColor(Color.RED);
 		//g2d.fillRect(game.randomTargetX(), 0, 10, 10);
-		g2d.fillRect((int)game.getTargetX(0), 0, 10, 10);
-		//}
-		/*if(game.getNumTurns() != 0){
-			g.setColor(Color.RED);
-			//g2d.fillRect(game.getTargetX(), 0, 10, 10);
-			g2d.fillRect(game.getTarget(), 0, 10, 10);
-		}*/
+		g2d.fill3DRect((int) game.getTargetX(0)+4, 0, 3, 30, true);
+		g.setColor(Color.GREEN);
+		g2d.fill3DRect((int) game.getTargetX(0)+4, 30, 20, 10, true);
 		g.setColor(Color.BLACK);
+		g2d.fillOval((int) (game.getTargetX(0)), 0, 10, 5);
 
 		//loops through the path 
 		for(int i = 0; i < game.getPathSize()-1; i++){
