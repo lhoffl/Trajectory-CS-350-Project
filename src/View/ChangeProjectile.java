cb180f4dafcf9306dad805ce5024674cb7fef9ab
Dfletcher06 <dfletcher06@gmail.com>
2015-12-09 19:05:16 +0000

    updated style guide

--- src/View/ChangeProjectile.java
+++ src/View/ChangeProjectile.java
@@ -15,26 +15,58 @@ import javax.swing.JTextField;
 
 import Model.Projectile;
 
+
+/**
+ * JDialog class that allows the user to change the projectile being launched.
+ * 
+ * @author Matthew Hoffman, Ian Mohr, David Fletcher
+ * @version Release Three | Last Updated: 12/09/2015
+ */
 public class ChangeProjectile extends JDialog implements ActionListener{
 
+	/** label that displays mass */
 	private JLabel massLabel;
+	
+	/** label that displays diameter */
 	private JLabel diameterLabel;
+	
+	/** label that displays name of projectile */
 	private JLabel nameLabel;
+	
+	/** textfield that allows for mass input */
 	private JTextField massBox;
+	
+	/** textfield that allows for diameter input */
 	private JTextField diameterBox;
+	
+	/** textfield that allows for name input */
 	private JTextField nameBox;
+	
+	/** creates the projectile when clicked */
 	private JButton create;
+	
+	/** list containing all the projectiles */
 	private JComboBox list;
+	
+	/** panel that contains the main components */
 	private JPanel panel;
+	
+	/** array of projectiles */
 	private Projectile[] projectileList = {Projectile.Default,
 			Projectile.BowlingBall,
 			Projectile.GolfBall,
 			Projectile.WaterMelon,
 			Projectile.Custom};
+			
+	/** instance of projectile class */
 	private Projectile projectile = null;
 
+	/**
+	 * Constructor that creates the projectile dialog box
+	 */
 	public ChangeProjectile(){
-
+	
+		//instantiations 
 		panel = new JPanel(new GridLayout(3, 2));
 		massLabel = new JLabel("Mass");
 		massLabel.setEnabled(false);
@@ -51,6 +83,7 @@ public class ChangeProjectile extends JDialog implements ActionListener{
 		create = new JButton("Create");
 		list = new JComboBox(projectileList);
 
+		//adding components
 		add(panel);
 		panel.add(nameLabel);
 		panel.add(massLabel);
@@ -61,6 +94,7 @@ public class ChangeProjectile extends JDialog implements ActionListener{
 		panel.add(list);
 		panel.add(create);
 
+		//super methods
 		setVisible(true);
 		pack();
 		setResizable(false);
@@ -69,11 +103,17 @@ public class ChangeProjectile extends JDialog implements ActionListener{
 
 		setLayout(new BorderLayout());
 
+		//action listeners
 		create.addActionListener(this);
 		list.addActionListener(this);
 	}
 
+	/**
+	 * Returns the projectile the user selected
+	 * @return Projectile that is selected
+	 */
 	public Projectile getProjectile(){
+	
 		for(int i = 0; i < projectileList.length; i++){
 			if(list.getSelectedItem() == Projectile.Custom){
 			}
@@ -84,6 +124,10 @@ public class ChangeProjectile extends JDialog implements ActionListener{
 		return projectile;
 	}
 
+	/**
+	 * ActionPerformed method that allows events
+	 * @param event for the action listener
+	 */
 	@Override
 	public void actionPerformed(ActionEvent event) {
 
@@ -109,6 +153,7 @@ public class ChangeProjectile extends JDialog implements ActionListener{
 			dispose();
 		}
 
+		//Custom
 		if(list.getSelectedItem() == Projectile.Custom){
 			massLabel.setEnabled(true);
 			massBox.setEnabled(true);
@@ -122,24 +167,28 @@ public class ChangeProjectile extends JDialog implements ActionListener{
 
 		}
 
+		//Default
 		if(list.getSelectedItem() == Projectile.Default){
 			nameBox.setText("Default");
 			massBox.setText("250");
 			diameterBox.setText(".5");
 		}
 
+		//Bowlingball
 		if(list.getSelectedItem() == Projectile.BowlingBall){
 			nameBox.setText("Bowling Ball");
 			massBox.setText("6.35");
 			diameterBox.setText(".218");
 		}
-
+		
+		//Golf ball
 		if(list.getSelectedItem() == Projectile.GolfBall){
 			nameBox.setText("Golf Ball");
 			massBox.setText(".046");
 			diameterBox.setText(".043");
 		}
-
+		
+		//Watermelon
 		if(list.getSelectedItem() == Projectile.WaterMelon){
 			nameBox.setText("Watermelon");
 			massBox.setText("10.0");
